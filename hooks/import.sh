086f9c1b3af6b0645b493463ae8b1a10247c79ae
CalebWebsterJCU <caleb.webster@my.jcu.edu.au>
2022-05-02 01:43:08 +1000

    Remove plugins and dumps

--- hooks/import.sh
+++ hooks/import.sh
@@ -1,23 +1,10 @@
 #!/usr/bin/sh
 
-types_string=$(../../php/php.exe wp-cli.phar post-type list --field=name)
+../../mysql/bin/mysql.exe jazztownsville < export/jazztownsville.sql -u root
 
 if [ $? != 0 ]; then
   echo "Could not import, Xampp is not running!"
   exit 1
 fi
-# Convert to array
-types=($types_string)
-
-for post_type in "${types[@]}"; do
-  ids=$(../../php/php.exe wp-cli.phar post list --post_type=$post_type --format=ids)
-  if [ -n "$ids" ]; then
-    ../../php/php.exe wp-cli.phar post delete $ids --force
-  fi
-done
-
-../../php/php.exe wp-cli.phar plugin activate wordpress-importer
-../../php/php.exe wp-cli.phar import export --authors=create
-../../php/php.exe wp-cli.phar theme activate understrap-child
 
 echo "Successfully imported WordPress site."
