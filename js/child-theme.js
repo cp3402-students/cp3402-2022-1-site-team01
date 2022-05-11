16ad2c220e64e0f0d21b95238e82802c2904134d
CalebWebsterJCU <caleb.webster@my.jcu.edu.au>
2022-05-11 19:05:59 +1000

    Change file structure

---
+++ js/child-theme.js
@@ -0,0 +1,14 @@
+/*!
+  * Understrap v1.1.0 (https://understrap.com)
+  * Copyright 2013-2022 The Understrap Authors (https://github.com/understrap/understrap/graphs/contributors)
+  * Licensed under GPL (http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+  */
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+})((function () { 'use strict';
+
+
+
+}));
+//# sourceMappingURL=child-theme.js.map
