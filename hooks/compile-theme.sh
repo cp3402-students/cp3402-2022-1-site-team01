af222870f9345c52b9dbfc8c4e121596c40d96c3
CalebWebsterJCU <caleb.webster@my.jcu.edu.au>
2022-05-11 10:33:24 +1000

    Edit theme style.css

--- hooks/compile-theme.sh
+++ hooks/compile-theme.sh
@@ -9,17 +9,16 @@ cp screenshot.png wp-content/themes/jazztownsville/screenshot.png
 cat <<EOT > wp-content/themes/jazztownsville/style.css
 /*
 Theme Name:   Jazz Townsville
-Theme URI:    https://understrap.com
+Theme URI:    https:/www.pccasegear.com
 Description:  Theme for Jazz Townsville website.
 Author:       Gauvan and the Boys
-Author URI:   https://github.com/understrap/understrap-child/graphs/contributors
-Template:     understrap
+Author URI:   https://github.com/cp3402-students/cp3402-2022-1-site-team01
 Version:      1.1.0
 License: GNU General Public License v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:  understrap-child
-Tags: one-column, custom-menu, featured-images, theme-options, translation-ready
-GitHub Theme URI: understrap/understrap-child
+Text Domain:  jazztownsville
+Tags: jazz
+GitHub Theme URI: cp3402-students/cp3402-2022-1-site-team01
 */
 EOT
 wp theme activate jazztownsville
