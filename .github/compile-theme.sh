d5265132bbbf3531c55f92bd76781b8d42242baa
CalebWebsterJCU <caleb.webster@my.jcu.edu.au>
2022-05-12 14:18:46 +1000

    Fix distributable theme title & iamge

--- .github/compile-theme.sh
+++ .github/compile-theme.sh
@@ -2,7 +2,7 @@ npm run dist
 npm run dist-build
 rm -r -f ../jazztheme
 mv dist ../jazztheme
+cd ../jazztheme
 cp .github/style.css style.css
-cp .github/screenshot.png screenshot.png
 cd ../../../  # jazztownsville (wp root)
 wp theme activate jazztheme
