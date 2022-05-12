2794b203f89b410c4482943132e0ef2bdb048927
CalebWebsterJCU <caleb.webster@my.jcu.edu.au>
2022-05-12 14:13:54 +1000

    Change image of child theme

--- .github/compile-theme.sh
+++ .github/compile-theme.sh
@@ -3,5 +3,6 @@ npm run dist-build
 rm -r -f ../jazztheme
 mv dist ../jazztheme
 cp .github/style.css style.css
+cp .github/screenshot.png screenshot.png
 cd ../../../  # jazztownsville (wp root)
 wp theme activate jazztheme
