cad7673547919546e35019b7efcb4fa60d0685b3
Gauvan Santiago <gauvan.santiago@my.jcu.edu.au>
2022-05-17 15:59:57 +1000

    removed comment section

--- single.php
+++ single.php
@@ -13,13 +13,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <div class="wrapper" id="single-wrapper">
-
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
-
 		<div class="row">
 
-
-			<main class="site-main" id="main" style="">
+			<main class="site-main" id="main">
 
 				<?php
 				while ( have_posts() ) {
@@ -27,21 +24,13 @@ $container = get_theme_mod( 'understrap_container_type' );
 					include 'content-single.php';
 					understrap_post_nav();
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
 				}
 				?>
 
 			</main><!-- #main -->
 
-			
-
 		</div><!-- .row -->
-
 	</div><!-- #content -->
-
 </div><!-- #single-wrapper -->
 
 <?php
