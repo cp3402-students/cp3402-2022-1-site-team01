840e3d0e3afa67bf92c16d6269f4b2f5653c2105
Gauvan Santiago <gauvan.santiago@my.jcu.edu.au>
2022-05-17 15:41:31 +1000

    removed left sidebar

--- single.php
+++ single.php
@@ -18,10 +18,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<div class="row">
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
-			<main class="site-main" id="main" style="background-color: red;">
+			<main class="site-main" id="main" style="">
 
 				<?php
 				while ( have_posts() ) {
